fb5d1407f543d630ae9b3265a1ee7474a1a41102
BrigethCastiblanco <b.castiblanco@uniandes.edu.co>
2022-08-20 21:06:24 -0500

    Shell punto 1

--- Taller1/Shell_1_factorial.sh
+++ Taller1/Shell_1_factorial.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-
-if [ $# -eq 0 ]; then
-	echo --- Programa sin parametros ---
-	echo --- Parametro 1: n-factorial ---
-	exit 1
-fi
-
-n=$1
-
-typeset -i factorial=1
-#!/bin/bash
-
-
-if [ $# -eq 0 ]; then
-	echo --- Programa sin parametros ---
-	echo --- Parametro 1: n-factorial ---
-	exit 1
-fi
-
-n=$1
+n=8
 
 typeset -i factorial=1
 
 if [ $n -eq 0 ] || [ $n -eq 1 ]; then
-	echo $1 $factorial 
+	echo El factorial de $n es $factorial 
 else
 	while [ $n -gt 1 ]
 	do
 		let factorial=$factorial*$n
 		let n=$n-1
 	done
-	echo  el factorial de $1 es $factorial
+	echo El factorial es $factorial
 fi
